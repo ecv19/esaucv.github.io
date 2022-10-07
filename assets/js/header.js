de49250429dd6d56bab100d65de5f01515034743
Juan Villela <juan@villela.co>
2022-10-07 16:28:58 -0400

    feat: Better header dropdown animations.

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,8 +1,10 @@
 let open = false;
+const header = document.querySelector('header');
 const nav = document.querySelector('nav#menu');
 const toggle = document.querySelector('button#menu-toggle');
 
 toggle.addEventListener('click', () => {
   open = !open;
+  header.setAttribute('data-active', open);
   nav.setAttribute('data-active', open);
 });
